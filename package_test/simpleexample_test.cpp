4b65e4fc2bdad27f103ebdc6e80d33848e0640b8
Masaaki HAMADA <msk24000@outlook.com>
2024-08-10 12:15:31 +0900

    package test

---
+++ package_test/simpleexample_test.cpp
@@ -0,0 +1,11 @@
+#include <simpleexample/simpleexample.hpp>
+#include <iostream>
+
+int main(int argc, char **argv) {
+    if(argc != 1) {
+        std::cout << argv[0] << " takes no arguments.\n";
+        return 1;
+    }
+    simpleexample::Simpleexample c;
+    return c.get_number() != 6;
+}
